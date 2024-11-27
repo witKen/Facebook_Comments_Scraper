6d9001c9792e7330e12c77f3e9d221d371d218a1
witKen <sokkenkh12@gmail.com>
2024-11-27 22:07:33 +0700

    Remove unused import

--- add_account.py
+++ add_account.py
@@ -1,5 +1,4 @@
-import os
-import pickle, pyfiglet
+import pickle
 import time
 
 with open('acc.txt', 'ab') as g:
--- scraping.py
+++ scraping.py
@@ -3,12 +3,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import threading
 import keyboard
-import pickle, pyfiglet
+import pickle
 
 # Path to your WebDriver (e.g., chromedriver)
 driver_path = "E:/Program Files/chromedriver-win64/chromedriver.exe"
